db82419be50eab0bf523a3be59e8c583912dd935
suos seyha <seyhasuos@gmail.com>
2023-06-21 17:34:22 +0700

    flutter

---
+++ android/app/src/main/kotlin/com/example/flutter_build_shopping_card1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_build_shopping_card1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/list_item.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_build_shopping_card1/item.dart';
+
+List<Item> products =[
+  Item(
+    name: 'Buger', 
+    unit: '1box', 
+    price: 10, 
+    image: 'assets/images/buger.jpg'
+  ),
+  Item(
+    name: 'Pizza', 
+    unit: 'Dox', 
+    price: 30, 
+    image: 'assets/images/pizza.jpg'
+  ),
+  Item(
+    name: 'Salmon', 
+    unit: 'kg', 
+    price: 18, 
+    image: 'assets/images/salmon.jpg'
+  ),
+  Item(
+    name: 'Tokoyaki', 
+    unit: '1box', 
+    price: 20, 
+    image: 'assets/images/tokoyaki.jpg'
+  ),
+];
